da352b1d499019a9bc9f953bcbde2f3b11c7397c
Anni Simonen <anni.simonen@helsinki.fi>
2024-01-26 18:01:45 +0200

    add db

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
-from flask import Flask,render_template, request, jsonify, session, redirect
+from flask import Flask,render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/get_points", methods = ["GET"])
+def get():
+    sql = text("SELECT id, latitude, longitude FROM point")
+    result = db.session.execute(sql)
+    points = result.fetchall()
+    points_data =[{'id': point.id, 'latitude': point.latitude, 'longitude': point.longitude} for point in points]
+    return jsonify({"points": points_data})
+
+
+@app.route("/create_point", methods=["POST"])
+def add():
+    try:
+        data = request.get_json()
+
+        latitude = data.get("latitude")
+        longitude = data.get("longitude")
+
+        sql = text("INSERT INTO point (latitude, longitude) VALUES (:latitude, :longitude) RETURNING id")
+        result = db.session.execute(sql, {"latitude": latitude, "longitude": longitude})
+        db.session.commit()
+
+        point_id = result.fetchone()[0]
+        return jsonify({"id": point_id})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
+@app.route("/point/<int:id>")
+def point(id):
+    sql = text("SELECT latitude, longitude FROM point WHERE id=:id")
+    result = db.session.execute(sql, {"id":id})
+    point = result.fetchall()
+    return render_template("point.html", id = id, point=point)
--- static/map.js
+++ static/map.js
@@ -1,9 +1,28 @@
-
-function setUp () {
+ function setUp () {
     const map = L.map('map').setView([60.1676, 24.94327], 13)
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '© OpenStreetMap contributors'
         }).addTo(map)
+
+        fetch('/get_points')
+        .then(response => response.json())
+        .then(data => {
+            const pointsData = data.points
+
+        pointsData.forEach(function(p){
+            const point = L.circle([p.latitude, p.longitude], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: 30
+            }).addTo(map)
+
+            const id = p.id
+            point.on('click', function(){
+                window.location.href ='point/' + id
+            })
+        })
+        })
 
         map.on('click', function(e) {
             const lat = e.latlng.lat
@@ -13,8 +32,24 @@ function setUp () {
                 color:'red',
                 fillColor: '#f03',
                 fillOpacity: 0.5,
-                radius:20
+                radius:30
             }).addTo(map)
+            
+            fetch('/create_point', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({latitude: lat, longitude: lng})
+            })
+            .then(response => response.json())
+            .then(data => {
+                const id = data.id
+                point.on('click', function() {
+                    window.location.href='point/' + id
+
+                })
+            })
 
         })
         return map
