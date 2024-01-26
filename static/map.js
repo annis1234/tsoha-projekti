ccb78bee12121cde95a57328110d8ad4421fe8d6
Anni Simonen <anni.simonen@helsinki.fi>
2024-01-26 15:39:41 +0200

    created map onClick functionality

--- static/map.js
+++ static/map.js
@@ -4,6 +4,19 @@ function setUp () {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '© OpenStreetMap contributors'
         }).addTo(map)
+
+        map.on('click', function(e) {
+            const lat = e.latlng.lat
+            const lng = e.latlng.lng
+            
+            const point = L.circle([lat, lng], {
+                color:'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius:20
+            }).addTo(map)
+
+        })
         return map
     }
 
